d3a505cf8085d9e42be37e1d4871a143b36cb91d
Randy2222222 <randyjpollitt@yahoo.com>
2026-02-03 23:35:37 -0500

    Update pdfReader.js

--- js/pdfReader.js
+++ js/pdfReader.js
@@ -117,8 +117,8 @@ if (DEV_MODE === "structured") {
       out += ` Equipment: ${pp.equipment}\n`
       out += `      Odds: ${pp.odds}\n`
       out += ` Finishers: ${pp.win.wn}${pp.win.lg}${pp.place.pl}${pp.place.lg}${pp.show.sh}${pp.show.lg}\n`
-   //   out += `  Comments: ${pp.comments}\n`
-     // out += `     Field: ${pp.field}\n`
+      out += `  Comments: ${pp.comments}\n`
+      out += `     Field: ${pp.field}\n`
     });
 
     out += "========================================\n\n";
